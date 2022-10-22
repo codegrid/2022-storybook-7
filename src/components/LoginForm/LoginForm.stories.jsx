b0abeec60a11a80f8645b9febe5dbab1de053265
tomof <fffjt007@gmail.com>
2022-10-22 19:25:40 +0900

    Interactionsのテストを追加

--- src/components/LoginForm/LoginForm.stories.jsx
+++ src/components/LoginForm/LoginForm.stories.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { LoginForm } from './LoginForm'
+// 👇テスト用のライブラリからimport
+import { userEvent, waitFor, within } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
 
 // コンポーネントの概要
 export default {
@@ -12,9 +15,24 @@ export default {
       },
     },
   },
+
+  // .storybook/preview.jsに、actions: { argTypesRegex: '^on[A-Z].*' },の指定があるので不要
+  // argTypes: {
+  //   onSubmit: { action: true },
+  // },
 }
 
 const Template = (args) => <LoginForm {...args} />
 
 export const Default = Template.bind({})
 Default.args = { ...Default.args }
+
+Default.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement)
+  console.log(canvas)
+  await userEvent.type(canvas.getByLabelText('メールアドレス'), 'tomof@example.com')
+  await userEvent.type(canvas.getByLabelText('パスワード'), 'supersecret')
+  await userEvent.click(canvas.getByRole('button'))
+
+  await waitFor(() => expect(args.onSubmit).toHaveBeenCalled())
+}
